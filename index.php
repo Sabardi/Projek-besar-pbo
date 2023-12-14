31c428182cca0f4106c36efb4e1211a1ece9320b
Sabardi <sabardibahari@gmail.com>
2023-12-14 12:27:36 +0800

    perubahan dikit

--- index.php
+++ index.php
@@ -6,47 +6,37 @@
     <title>Sevice computer</title>
     <link rel="shortcut icon" href="asset/img/logo service.png" type="image/x-icon">
     <link rel="stylesheet" href="asset/css/bootstrap.min.css">
-    <style>
-        body {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          height: 100vh;
-          margin: 0;
-        }
-      </style>
 </head>
 <body>
-
-<main class="form-signin">
+<main class="mdc-layout-grid">
     <div class="container">
-    <div class="d-flex align-items-center">
-        <form>
-            <img class="mb-4" src="asset/img/logo service.png" alt="" width="500px">
-            <h1 class="h3 mb-3 fw-normal">silahkan login</h1>
-        
-            <div class="form-floating">
-              <input type="email" class="form-control" id="floatingInput" placeholder="email@gmail.com">
-              <label for="floatingInput">Email anda</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="masukan Password anda">
-              <label for="floatingPassword">Password</label>
-            </div>
-        
-            <div class="form-check text-start my-3">
-              <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-              <label class="form-check-label" for="flexCheckDefault">
-                Ingatkan saya
-              </label>
-            </div>
-            <a href="/index.php" class="btn btn-primary w-100 py-2">login</a>
-                <!-- <button class="btn btn-primary w-100 py-2" type="submit">>login</button> -->
-          </form>
-        </div>
-        </div>
-</main>
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-6">
+                <form>
+                    <img class="mb-4 img-fluid" src="asset/img/logo service.png" alt="" width="500px">
+                    <h1 class="h3 mb-3 fw-normal">Silahkan Login</h1>
 
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="email@gmail.com">
+                        <label for="floatingInput">Email Anda</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="masukan Password Anda">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+
+                    <div class="form-check text-start my-3">
+                        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Ingatkan Saya
+                        </label>
+                    </div>
+                    <a href="index.php" class="btn btn-primary w-100 py-2">Login</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>
 
