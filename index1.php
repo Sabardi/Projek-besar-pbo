506e01dc7dd7e1a346716bf4610a1042ddf96622
Sabardi <sabardibahari@gmail.com>
2023-12-14 08:42:11 +0800

    d

---
+++ index1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sevice computer</title>
+    <link rel="shortcut icon" href="img/logo service.png" type="image/x-icon">
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div>
+                gambar
+                <a href="">logo</a>
+            </div>
+        </div>
+    </header>
+
+    <!-- content section -->
+    <section>
+        <div class="container">
+            <a href="./frontend/login.php">ke bagian login </a>
+        </div>
+    </section>
+</body>
+</html>
